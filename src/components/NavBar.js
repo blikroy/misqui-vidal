a74930f48f6a3f3c20f54ddcb2e11f144ee9aa26
blikroy <elroyvidal@gmai.com>
2022-08-30 22:02:25 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return(
+        <header>
+            <div className="logo">MISQUI</div>
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Categoría</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
