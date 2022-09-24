91b197095c210be60c369194f2bd164273360eea
blikroy <elroyvidal@gmai.com>
2022-09-23 23:21:42 -0300

    CartContex

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,30 +7,30 @@ const Cart = () => {
         <>
             <h1>Mi carrito</h1>
             <table width='400px'>
-            <tr>
-                <th>Nombre</th>
-                <th>Cantidad</th>
-                <th>Precio</th>
-                <th>Acción</th>
-            </tr>
-            {
-                carrito.cartList.map(item => 
-                    <tr key={item.id}>
-                        <td>{item.nombre}</td>
-                        <td>{item.cantidad}</td>
-                        <td>{item.cantidad * item.precio}</td>
-                        <td><button onClick={() => carrito.removeItem(item.id)}>Borrar</button></td>
-                    </tr>                    
-                )
-            }
-            <br/>
-            <tr>
-            <td><button onClick={carrito.clear}>Borrar todo el carrito</button></td>
-            <td></td>
-            <td></td>
-            <td></td>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Cantidad</th>
+                    <th>Precio</th>
+                    <th>Acción</th>
+                </tr>
+                {
+                    carrito.cartList.map(item => 
+                        <tr key={item.id}>
+                            <td>{item.nombre}</td>
+                            <td>{item.cantidad}</td>
+                            <td>{item.cantidad * item.precio}</td>
+                            <td><button onClick={() => carrito.removeItem(item.id)}>Borrar</button></td>
+                        </tr>                    
+                    )
+                }
+                <br/>
+                <tr>
+                    <td><button onClick={carrito.clear}>Borrar todo el carrito</button></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
 
-            </tr>
+                </tr>
             </table>
         </>
     );
--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -7,16 +7,42 @@ const CartContextProvider = ({children}) => {
     const [cartList, setCartList] = useState([])
 
     const addItem = (contenidos, cantidad) => {
-        const listaCantidad = {
-            ...contenidos, cantidad: cantidad
+        //busco en mi array de CartList con find si el id que voy a cargar ya se encuentra o no
+       
+
+        //si no lo encontro que cargue normal
+        if(cartList.find(c => c.id === contenidos.id)) {
+             //averiguamos posición del objeto en el array
+             const index = cartList.findIndex(p => p.id === contenidos.id);
+
+             //buscamos solo el objeto con la posición
+             const producto = cartList[index];
+ 
+             //le sumo la cantidad nueva a la cantidad que ya se encontraba en el carrito
+             producto.cantidad = producto.cantidad + cantidad;
+ 
+             //creo un nuevo array ya con los datos modificados del carrito anterior
+             const newCart = [...cartList];
+ 
+             //al nuevo carrito le agrego todos los cambios que hice en cantidad
+             newCart.splice(index, 1, producto);
+ 
+             //seteo el nuevo array a mi useState.
+             setCartList([...newCart]);
+        }else {
+            const listaCantidad = {
+                ...contenidos, cantidad: cantidad
+            }
+            setCartList([...cartList, listaCantidad])
+           
+
         }
-        setCartList([...cartList, listaCantidad])
-    }
-    const clear = ()=> {
-        setCartList([]);
     }
     const removeItem = (id) => {
         setCartList(cartList.filter(item => item.id !== id))
+    }
+    const clear = ()=> {
+        setCartList([]);
     }
     return(
         <CartContext.Provider value={{cartList, addItem, clear, removeItem}}>
