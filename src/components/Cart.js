24e4302594174145e6e5a6e998c0bdf2b203ed90
blikroy <elroyvidal@gmai.com>
2022-10-04 23:51:11 -0300

    firebase II

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,31 @@
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "./CartContext";
 
 const Cart = () => {
     const carrito = useContext(CartContext)
+
+    const orden = {
+        usuario: {
+            nombre: 'roy',
+            apellido: 'vidal',
+            email: 'royvidal@email.com',
+            direccion: 'calle falsa - 123',
+            telefono: 1122334455
+        },
+        producto: carrito.cartList.map(item => ({id: item.id, nombre: item.nombre, precio: item.precio, cantidad: item.cantidad})),
+        total: carrito.precioTotal(),
+    }
+
+    const finalizar = () => {
+        const db = getFirestore();
+        const ordenes = collection(db, 'ordenes');
+        addDoc(ordenes, orden);
+        carrito.clear()
+    }
+
+
     if(carrito.cartList.length === 0) {
         return(
             <>
@@ -37,12 +59,12 @@ const Cart = () => {
                     <td>Total de la compra: {carrito.precioTotal()}</td>
                     <td></td>
                     <td></td>
-                    <td></td>
+                    <td><button onClick={carrito.clear}>Borrar todo el carrito</button></td>
 
                 </tr>
                 <br/>
                 <tr>
-                    <td><button onClick={carrito.clear}>Borrar todo el carrito</button></td>
+                    <td><button onClick={finalizar}>Finalizar Compra</button></td>
                     <td></td>
                     <td></td>
                     <td></td>
